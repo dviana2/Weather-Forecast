f97783874d79b43c94bcd94266e427389fcb6bca
Dayry Viana <dayry.viana@gmail.com>
2021-10-14 21:04:25 -0400

    Uploading Weather Forecast

---
+++ actions.js
@@ -0,0 +1,10 @@
+console.log("JavaScript file connected");
+
+function loading() {
+    alert ("Loading weather report...")
+}
+
+function acceptCookies() {
+    var accept = document.querySelector(".cookie-policy")
+    accept.remove()
+}
